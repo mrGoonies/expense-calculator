c213be00ae6f6f3fd593dd198e25cabebc441648
mrGoonies <gooniesdev@gmail.com>
2023-12-16 22:04:42 -0300

    Add: User enter their income

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ Expense Calculator App
 from typing import List
 
 
+list_of_incomes: List[int] = list()
+
+
+def enter_income_amounts(amount_of_income: int) -> List[int]:
+    """
+    Enter the amount of income from the user
+    :param amount_of_income: The amount of income as an integer
+    :return: A list of income amounts
+    """
+
+    for amount in range(amount_of_income):
+        income = int(input("Enter income amount:\n>>> "))
+        assert income > 0, "Income must be greater than 0"
+
+        list_of_incomes.append(income)
+    return list_of_incomes
+
+
 def get_amount_of_income() -> int:
     """
     Get the amount of income from the input user
@@ -22,4 +40,6 @@ if __name__ == '__main__':
     print("-" * 30)
     print("\t\tExpense Calculator App")
     print("-" * 30)
-    get_amount_of_income()
+
+    income_amount = get_amount_of_income()
+    income_list: List[int] = enter_income_amounts(income_amount)
